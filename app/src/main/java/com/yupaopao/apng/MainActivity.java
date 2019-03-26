cf7fc84a5bedb6bc0d431ae21a75b09e49f3182e
Zhou Pengfei <pengfeizhou@foxmail.com>
2019-03-26 20:35:20 +0800

    init commit

---
+++ app/src/main/java/com/yupaopao/apng/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yupaopao.apng;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
