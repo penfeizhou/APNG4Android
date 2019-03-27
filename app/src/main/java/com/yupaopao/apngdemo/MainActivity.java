40c528187365dd4b01e47fa03af1648e7f59e155
Zhou Pengfei <pengfeizhou@foxmail.com>
2019-03-27 10:00:35 +0800

    feat:完善demo

--- app/src/main/java/com/yupaopao/apngdemo/MainActivity.java
+++ app/src/main/java/com/yupaopao/apngdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.yupaopao.apngdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.yupaopao.apng.ApngImageUtil;
@@ -15,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ApngLoader.init(getApplicationContext());
         ApngLoader.loadImage(ApngImageUtil.Scheme.ASSETS.wrap("sample.png"), (ImageView) findViewById(R.id.imageView), null);
+        final ImageView imageView = findViewById(R.id.imageView2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ApngLoader.loadRepeatCountImage(
+                        ApngImageUtil.Scheme.ASSETS.wrap("feeds_apng_like_red.png"), imageView, 1, null);
+            }
+        });
     }
 }
