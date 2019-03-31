cf054fb064117a6a82d39e6a1e7fb2e5f5eede03
Zhou Pengfei <pengfeizhou@foxmail.com>
2019-03-31 14:09:15 +0800

    demo

--- app/src/main/java/com/yupaopao/apngdemo/APNGLibActivity.java
+++ app/src/main/java/com/yupaopao/apngdemo/APNGLibActivity.java
@@ -2,14 +2,12 @@ package com.yupaopao.apngdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.yupaopao.animation.apng.APNGAssetLoader;
 import com.yupaopao.animation.apng.APNGDrawable;
-import com.yupaopao.animation.apng.APNGFileLoader;
-import com.yupaopao.animation.apng.APNGResourceLoader;
-import com.yupaopao.animation.apng.chunk.APNGDecoder;
 
 /**
  * @Description: 作用描述
@@ -21,29 +19,23 @@ public class APNGLibActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apnglib);
-        final ImageView imageView = findViewById(R.id.imageView);
-        final APNGAssetLoader assetLoader = new APNGAssetLoader(this, "wheel.png");
-        final APNGResourceLoader resourceLoader = new APNGResourceLoader(APNGLibActivity.this, R.drawable.sample);
-        final APNGFileLoader fileLoader = new APNGFileLoader("/sdcard/Pictures/wheel.png");
-        imageView.setImageDrawable(
-                new APNGDrawable(assetLoader));
-
-        final ImageView imageView2 = findViewById(R.id.imageView2);
-        imageView2.setImageDrawable(
-                new APNGDrawable(assetLoader));
-
-        final ImageView imageView3 = findViewById(R.id.imageView3);
-        imageView3.setImageDrawable(
-                new APNGDrawable(assetLoader));
+        LinearLayout linearLayout = findViewById(R.id.layout);
+        String[] assetFiles = {
+                "test1.png",
+                "test2.png",
+                "test3.png",
+                "test4.png",
+                "wheel.png",
+        };
+        for (String assetFile : assetFiles) {
+            APNGAssetLoader loader = new APNGAssetLoader(this, assetFile);
+            APNGDrawable apngDrawable = new APNGDrawable(loader);
+            ImageView imageView = new ImageView(this);
+            imageView.setImageDrawable(apngDrawable);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.bottomMargin = 50;
+            layoutParams.topMargin = 50;
+            linearLayout.addView(imageView, layoutParams);
+        }
     }
-//    imageView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                imageView.setImageDrawable(
-//                        new APNGDrawable(
-//                                new APNGFileLoader("/sdcard/Pictures/wheel.png"),
-//                                true,
-//                                APNGDecoder.Mode.MODE_BALANCED));
-//            }
-//        });
 }
