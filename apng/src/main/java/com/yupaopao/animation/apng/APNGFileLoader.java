35ce6ca22dd192ba7fc6972ced237b8d81578405
Zhou Pengfei <pengfeizhou@foxmail.com>
2019-05-14 13:34:09 +0800

    feat:compat old version

---
+++ apng/src/main/java/com/yupaopao/animation/apng/APNGFileLoader.java
@@ -0,0 +1,16 @@
+package com.yupaopao.animation.apng;
+
+import com.yupaopao.animation.loader.FileStreamLoader;
+
+/**
+ * @Author: pengfei.zhou
+ * @CreateDate: 2019/3/28
+ * @see com.yupaopao.animation.loader.FileStreamLoader use this insted
+ */
+@Deprecated
+public class APNGFileLoader extends FileStreamLoader {
+
+    public APNGFileLoader(String path) {
+        super(path);
+    }
+}
