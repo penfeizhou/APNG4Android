19ef761113626cdb46fc973833297431b2fe34d1
Zhou Pengfei <pengfeizhou@foxmail.com>
2019-05-19 17:16:06 +0800

    feat:实现GIF中LZW解压算法

--- gif/src/main/java/com/yupaopao/animation/gif/io/GifWriter.java
+++ gif/src/main/java/com/yupaopao/animation/gif/io/GifWriter.java
@@ -2,11 +2,22 @@ package com.yupaopao.animation.gif.io;
 
 import com.yupaopao.animation.io.ByteBufferWriter;
 
+import java.nio.ByteBuffer;
+
 /**
  * @Description: APNG4Android
  * @Author: pengfei.zhou
  * @CreateDate: 2019-05-16
  */
 public class GifWriter extends ByteBufferWriter {
+    public void putInt(int val) {
+        putByte((byte) (val & 0xff));
+        putByte((byte) ((val >> 8) & 0xff));
+        putByte((byte) ((val >> 16) & 0xff));
+        putByte((byte) ((val >> 24) & 0xff));
+    }
 
+    public ByteBuffer toByteBuffer() {
+        return byteBuffer;
+    }
 }
