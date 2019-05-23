7578c78521ad989b6d80cf788e4a65cbd07e78f7
Zhou Pengfei <pengfeizhou@foxmail.com>
2019-05-23 21:03:24 +0800

    feat:fix file loader cannot reset inputstream

---
+++ frameanimation/src/main/java/com/yupaopao/animation/loader/FileLoader.java
@@ -0,0 +1,38 @@
+package com.yupaopao.animation.loader;
+
+
+import com.yupaopao.animation.io.Reader;
+import com.yupaopao.animation.io.FileReader;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @Description: 从文件加载流
+ * @Author: pengfei.zhou
+ * @CreateDate: 2019/3/28
+ */
+public class FileLoader implements Loader {
+
+    private final File mFile;
+    private Reader mReader;
+
+    public FileLoader(String path) {
+        mFile = new File(path);
+    }
+
+    @Override
+    public synchronized Reader obtain() throws IOException {
+        if (mReader == null) {
+            mReader = new FileReader(mFile);
+        }
+        return mReader;
+    }
+
+    @Override
+    public void release() throws IOException {
+        if (mReader != null) {
+            mReader.close();
+        }
+    }
+}
