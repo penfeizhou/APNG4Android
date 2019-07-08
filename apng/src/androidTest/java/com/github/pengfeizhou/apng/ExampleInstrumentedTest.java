ccbfc16b77ea369014bad6650c3362e85124b8a2
Zhou Pengfei <pengfeizhou@foxmail.com>
2019-07-08 17:04:39 +0800

    clear domain

--- apng/src/androidTest/java/com/github/pengfeizhou/apng/ExampleInstrumentedTest.java
+++ apng/src/androidTest/java/com/github/pengfeizhou/apng/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.yupaopao.apng.test", appContext.getPackageName());
+        assertEquals("com.github.pengfeizhou.apng.test", appContext.getPackageName());
     }
 }
