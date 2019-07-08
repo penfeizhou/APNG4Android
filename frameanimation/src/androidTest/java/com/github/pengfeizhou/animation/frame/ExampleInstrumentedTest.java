390a2e469a98e339bc536cc94a9df521a968e9cf
周鹏飞 <zhoupengfei@yupaopao.cn>
2019-07-08 17:04:39 +0800

    clear domain

--- frameanimation/src/androidTest/java/com/github/pengfeizhou/animation/frame/ExampleInstrumentedTest.java
+++ frameanimation/src/androidTest/java/com/github/pengfeizhou/animation/frame/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.yupaopao.animation.frame.test", appContext.getPackageName());
+        assertEquals("com.github.pengfeizhou.animation.frame.test", appContext.getPackageName());
     }
 }
--- gif/src/androidTest/java/com/github/pengfeizhou/animation/gif/ExampleInstrumentedTest.java
+++ gif/src/androidTest/java/com/github/pengfeizhou/animation/gif/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.yupaopao.animation.gif.test", appContext.getPackageName());
+        assertEquals("com.github.pengfeizhou.animation.gif.test", appContext.getPackageName());
     }
 }
