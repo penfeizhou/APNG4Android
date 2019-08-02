7e213cabe27d8a6aa75ed2c190d19b80b3f3402a
周鹏飞 <zhoupengfei@yupaopao.cn>
2019-08-02 15:58:24 +0800

    gif to webp support

--- app/src/main/java/com/github/penfeizhou/animation/demo/APNGTestActivity.java
+++ app/src/main/java/com/github/penfeizhou/animation/demo/APNGTestActivity.java
@@ -1,13 +1,20 @@
 package com.github.penfeizhou.animation.demo;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.github.penfeizhou.animation.glide.AnimationDecoderOption;
+import com.bumptech.glide.Glide;
+import com.github.penfeizhou.animation.awebpencoder.WebPEncoder;
+import com.github.penfeizhou.animation.loader.AssetStreamLoader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 
 /**
  * @Description: 作用描述
@@ -20,32 +27,26 @@ public class APNGTestActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apnglib);
         LinearLayout linearLayout = findViewById(R.id.layout);
-        String[] urls = new String[]{
-                "https://misc.aotu.io/ONE-SUNDAY/SteamEngine.png",
-                "https://isparta.github.io/compare-webp/image/gif_webp/webp/2.webp",
-                "file:///android_asset/1.gif",
-                "file:///android_asset/5.gif",
-                "file:///android_asset/6.gif",
-        };
-        for (String url : urls) {
-            ImageView imageView = new ImageView(this);
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            layoutParams.bottomMargin = 50;
-            layoutParams.topMargin = 50;
-            linearLayout.addView(imageView, layoutParams);
-            GlideApp.with(imageView)
-                    .load(url)
-                    .set(AnimationDecoderOption.DISABLE_ANIMATION_GIF_DECODER, false)
-                    .into(imageView);
-        }
-//        FileLoader fileLoader = new FileLoader("/data/data/com.github.penfeizhou.animation.demo/cache/image_manager_disk_cache/b6dd68d837b8d1e8f24edb0a0df5213b7accebb16c22c8202e95363f7227740e.0");
-//
-//        ImageView imageView = new ImageView(this);
-//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//        layoutParams.bottomMargin = 50;
-//        layoutParams.topMargin = 50;
-//        linearLayout.addView(imageView, layoutParams);
-//        imageView.setImageDrawable(new APNGDrawable(fileLoader));
+        final ImageView imageView = new ImageView(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.bottomMargin = 50;
+        layoutParams.topMargin = 50;
+        linearLayout.addView(imageView, layoutParams);
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                WebPEncoder webPEncoder = new WebPEncoder();
+                final ByteBuffer ret = webPEncoder.fromGif(new AssetStreamLoader(APNGTestActivity.this, "2.gif"));
+                imageView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(imageView)
+                                .load(ret.array())
+                                .into(imageView);
+                    }
+                });
+            }
+        }).start();
     }
 }
