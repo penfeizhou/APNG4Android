b5ca5cc3e1f6c09c30be44eaaf1a74155ab060e5
pengfei.zhou <pengfeizhou@foxmail.com>
2022-05-26 13:41:42 +0800

    GIF: add protection for gif's Background Color Index out of range

--- gif/src/main/java/com/github/penfeizhou/animation/gif/decode/GifDecoder.java
+++ gif/src/main/java/com/github/penfeizhou/animation/gif/decode/GifDecoder.java
@@ -95,7 +95,7 @@ public class GifDecoder extends FrameSeqDecoder<GifReader, GifWriter> {
         }
         frameBuffer = ByteBuffer.allocate((canvasWidth * canvasHeight / (sampleSize * sampleSize) + 1) * 4);
         snapShot.byteBuffer = ByteBuffer.allocate((canvasWidth * canvasHeight / (sampleSize * sampleSize) + 1) * 4);
-        if (globalColorTable != null && bgColorIndex > 0) {
+        if (globalColorTable != null && bgColorIndex >= 0 && bgColorIndex < globalColorTable.getColorTable().length) {
             int abgr = globalColorTable.getColorTable()[bgColorIndex];
             this.bgColor = Color.rgb(abgr & 0xff, (abgr >> 8) & 0xff, (abgr >> 16) & 0xff);
         }
