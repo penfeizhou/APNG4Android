6d58278585e4b92e026adfe320e4fb537223d7b1
pengfei.zhou <pengfeizhou@foxmail.com>
2023-09-06 14:07:55 +0800

    Add demo to use glide in compose ui

---
+++ app/src/main/java/com/github/penfeizhou/animation/demo/ComposeActivity.kt
@@ -0,0 +1,57 @@
+package com.github.penfeizhou.animation.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+
+
+/**
+ *
+ * @Description:    ComposeActivity
+ * @Author:         pengfei.zhou
+ * @CreateDate:     2023/9/6
+ */
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimationDemo()
+        }
+    }
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun AnimationDemo() {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(20.dp)
+    ) {
+        GlideImage(
+            model = "file:///android_asset/1.webp",
+            contentDescription = "Test",
+        )
+        GlideImage(
+            model = "file:///android_asset/test1.png",
+            contentDescription = "Test",
+        )
+        GlideImage(
+            model = "file:///android_asset/2.gif",
+            contentDescription = "Test",
+        )
+    }
+}
