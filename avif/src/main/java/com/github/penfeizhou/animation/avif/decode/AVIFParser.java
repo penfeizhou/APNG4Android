743190c40de70d239d82cd293cdc0aaab02a57d3
pengfei.zhou <pengfeizhou@foxmail.com>
2023-09-11 11:14:20 +0800

    stash files

--- avif/src/main/java/com/github/penfeizhou/animation/avif/decode/AVIFParser.java
+++ avif/src/main/java/com/github/penfeizhou/animation/avif/decode/AVIFParser.java
@@ -1,6 +1,7 @@
 package com.github.penfeizhou.animation.avif.decode;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import com.github.penfeizhou.animation.avif.io.AVIFReader;
 import com.github.penfeizhou.animation.io.Reader;
@@ -9,6 +10,8 @@ import com.github.penfeizhou.animation.io.StreamReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @link {https://aomediacodec.github.io/av1-spec/av1-spec.pdf}
@@ -84,5 +87,63 @@ public class AVIFParser {
         } catch (IOException e) {
             return false;
         }
+    }
+
+    public static List<Box> parse(AVIFReader reader) throws IOException {
+        List<Box> boxes = new ArrayList<>();
+        while (reader.available() > 0) {
+            boxes.add(parseBox(reader));
+        }
+        return boxes;
+    }
+
+    public static boolean matchFourCC(String chars, int fourCC) throws IOException {
+        if (TextUtils.isEmpty(chars) || chars.length() != 4) {
+            return false;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (((fourCC >> (i * 8)) & 0xff) != chars.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static Box parseBox(AVIFReader reader) throws IOException {
+        int offset = reader.position();
+        int size = reader.readUInt32();
+        String type = reader.readString(4);
+        Box box;
+        switch (type) {
+            case FileTypeBox.ID:
+                box = new FileTypeBox();
+                break;
+            case MetaBox.ID:
+                box = new MetaBox();
+                break;
+            case HandlerBox.ID:
+                box = new HandlerBox();
+                break;
+            case PrimaryItemBox.ID:
+                box = new PrimaryItemBox();
+                break;
+            case ItemLocationBox.ID:
+                box = new ItemLocationBox();
+                break;
+            case ItemInformationBox.ID:
+                box = new ItemInformationBox();
+                break;
+            case ItemInformationBox.INFE_ID:
+                box = new ItemInformationBox.ItemInfoEntry();
+                break;
+            default:
+                box = new Box();
+                break;
+        }
+        box.offset = offset;
+        box.size = size;
+        box.type = type;
+        box.parse(reader);
+        return box;
     }
 }
